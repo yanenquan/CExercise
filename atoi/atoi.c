6a445ea0a0fa73cabb1f9aba694bc31ba27c916a
781934291@qq.com <781934291@qq.com>
2014-04-26 19:24:01 +0800

    提交

---
+++ atoi/atoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int atoi(const char *s)
+{
+	char *p = s;
+	char c;
+	int result = 0;
+	while(c = *p++)
+	{
+		if((c >= '0') && (c <= '9'))
+		{
+			  result = result*10 + (c - '0');
+		}
+		else
+			  return -1;                     //Invalid   string
+	}
+	return   i;
+	} 
+
+
+int main()
+{
+	char str1 = "1234";
+	printf("%d\n",atoi(str1);
+	return 0;
+} 
---
+++ strcmp/my_strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int my_strcmp(const char *str1, const char *str2)
+{
+   assert((NULL != str1) && (NULL != str2));
+   while (*str1 && *str2 && (*str1 == *str2))
+   {
+		str1++;
+        str2++;
+   }
+   
+   /*如果str1 > str2,则返回值大于0，如果str1 = str2,则返回值等于0，如果str1  < str2 ,则返回值小于0。*/
+   return *str1 - *str2;
+   
+}
+
+
+int main()
+{
+	char str1 = "hellojava";
+	char str2 = "hello";
+	printf("%d\n",my_strcmp(str1,str2);
+	return 0;
+} 
