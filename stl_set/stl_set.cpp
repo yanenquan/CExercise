d805cf9bb4e301e739ca9e5b7c19e28ade86549a
781934291@qq.com <781934291@qq.com>
2014-04-29 20:34:35 +0800

    STL set 操作

---
+++ stl_set/stl_set.cpp
@@ -0,0 +1,43 @@
+// constructing sets
+#include <iostream>
+#include <set>
+
+bool fncomp (int lhs, int rhs) {return lhs<rhs;}
+
+struct classcomp {
+  bool operator() (const int& lhs, const int& rhs) const
+  {return lhs<rhs;}
+};
+
+int main ()
+{
+  std::set<int> first;                           // empty set of ints
+
+  int myints[]= {10,20,30,40,50};
+  std::set<int> second (myints,myints+5);        // range
+  //find and delete
+  std::set<int>::iterator it;
+  it = second.find (50);
+  second.erase (it);
+  
+  //insert
+  second.insert(1);
+  
+
+  std::set<int> third (second);                  // a copy of second
+
+  std::set<int> fourth (second.begin(), second.end());  // iterator ctor.
+
+  bool(*fn_pt)(int,int) = fncomp;
+  std::set<int,bool(*)(int,int)> sixth (fn_pt);  // function pointer as Compare
+	  
+  std::set<int,classcomp> fifth(myints,myints+5);                 // class as Compare
+  std::set<int,classcomp>::iterator iter = fifth.begin();
+  while(iter != fifth.end())
+  {
+	  std::cout<<*iter<<std::endl;
+	  iter++;
+  }
+
+  return 0;
+}
