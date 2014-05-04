44f90b16d6c21f9d4874ffeb323e5be77fa532be
781934291@qq.com <781934291@qq.com>
2014-05-04 16:59:29 +0800

    合并排序

--- mergesort/mergesort.cpp
+++ mergesort/mergesort.cpp
@@ -1,41 +1,70 @@
+/******************************************************************************
+*  author: yanenquan
+*  version: 1.0
+*  description: 合并排序
+*  date: 2014-04-11
+*  other: 无
+******************************************************************************/
+
 #include <iostream>  
 using namespace std;  
 
 template <class T>   
 void show(T arr,int n){  
-    for(int i =0;i<n-1;i++){  
+	
+    for(int i =0;i<n-1;i++)
+	{  
         cout<<arr[i]<<",";  
     }  
+	
     cout<<arr[n-1]<<endl;  
-}  
+} 
+
 /* 将有序的SR[i..m]和SR[m+1..n]归并为有序的TR[i..n] */  
 void Merge(int SR[],int TR[],int i,int m,int n)  
 {  
 	int j,k,l;  
-	for(j=m+1,k=i;i<=m && j<=n;k++) /* 将SR中记录由小到大归并入TR */  
+	
+	/* 将SR中记录由小到大归并入TR */
+	for(j=m+1,k=i;i<=m && j<=n;k++)   
 	{  
 		if(SR[i]<SR[j])
-			TR[k]=SR[i++];  
+		{
+			TR[k]=SR[i++]; 
+		}
 		else  
+		{
 			TR[k]=SR[j++];  
+		}
 	}  
+	
+	/* 将剩余的SR[i..m]复制到TR */  
 	if(i<=m)
 	{  
 		for(l=0;l<=m-i;l++) 
-		TR[k+l]=SR[i+l]; /* 将剩余的SR[i..m]复制到TR */  
+		{
+		    TR[k+l]=SR[i+l]; 
+		}
 	}  
+	
+	/* 将剩余的SR[j..n]复制到TR */  
 	if(j<=n)  
 	{  
 		for(l=0;l<=n-j;l++)
-		TR[k+l]=SR[j+l]; /* 将剩余的SR[j..n]复制到TR */  
+		{
+		    TR[k+l]=SR[j+l]; 
+		}
 	}  
 }
+
 void MSort(int SR[],int TR1[],int s, int t)  
 {  
 	int m;  
 	int TR2[10];  
-	if(s==t)  
+	if(s==t) 
+	{
 		TR1[s]=SR[s];  
+	}
 	else  
 	{  
 		m=(s+t)/2; /* 将SR[s..t]平分为SR[s..m]和SR[m+1..t] */  
